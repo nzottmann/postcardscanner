7bde02737819a3e3a7e50243bb24bd77de6d5065
Nils Zottmann <mail@nils-zottmann.de>
2022-09-09 10:36:13 +0200

    Add GET last_postcard_timestamp #1

--- app/main.py
+++ app/main.py
@@ -3,6 +3,10 @@ from postcardscanner.hardware.scanner_v1 import ScannerV1
 
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+import base64
+import os
 
 def callback(image):
     print('Received image')
@@ -15,11 +19,34 @@ scanner.start()
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+                   )
+
 
 @app.get("/")
 async def root():
-    return {"message": "Get last postcard from /last_postcard"}
-
+    return os.path.getctime('img.jpg')
+    
 @app.get("/last_postcard")
 async def root():
-    return FileResponse('img.jpg')
+    with open('img.jpg', 'rb') as f:
+        base64image = base64.b64encode(f.read())
+    return base64image
+   #return FileResponse('img.jpg')
+
+@app.get("last_postcard")
+async def root():
+    with open('img.jpg', 'rb') as f:
+        base64image = base64.b64encode(f.read())
+    return base64image
+
+@app.get("last_postcard_timestamp")
+async def root():
+    return os.path.getctime('img.jpg')
