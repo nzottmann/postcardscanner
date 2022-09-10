fb647a3cef13f855df9a2b7a57ea2adfb881998e
Nils Zottmann <n.zottmann@mozys.de>
2022-09-10 18:48:53 +0200

    Formatting

--- app/main.py
+++ app/main.py
@@ -19,15 +19,13 @@ scanner.start()
 
 app = FastAPI()
 
-origins = ["*"]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-                   )
+)
 
 
 @app.get("/")
